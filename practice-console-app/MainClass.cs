b0ee1231e51eb430771e7a95bcf4303a9e1e7804
vladm <vlad.a.monea@gmail.com>
2026-05-10 03:02:50 +0300

    Added gitignore and created a basic console app

---
+++ practice-console-app/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice_console_app
+{
+    internal class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
